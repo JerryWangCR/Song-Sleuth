8f6d69b3c54ca16a4d1ece96aa78d5605aaa64d5
JerryWangCR <jerry.cr.w@gmail.com>
2023-04-16 11:18:55 -0400

    Feat(Game Mode Screen) : created prototype of Game Mode Selection Screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./src/Screens/GameModeScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hello welcome to SONG SLEUTH</Text>
+    <NavigationContainer>
+      {/* <View style={styles.container}> */}
+      {/* <Text>hello welcome to SONG SLEUTH</Text> */}
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        {/* <Stack.Screen name="Settings" component={SettingScreen} /> */}
+      </Stack.Navigator>
       <StatusBar style="auto" />
-    </View>
+    {/* </View> */}
+    </NavigationContainer>
   );
 }
 
