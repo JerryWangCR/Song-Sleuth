35566e0c215c34507a4997a46fe30d6d2b7bc954
kenyrishi <kenyrishi@gmail.com>
2023-05-02 05:37:31 -0400

    Cleaned Up End Screen

--- src/Screens/EndScreen.tsx
+++ src/Screens/EndScreen.tsx
@@ -1,22 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
-import styles from './EndScreenStyles';
 
-const MyPage: React.FC = () => {
-  const [counter, setCounter] = useState(0);
-
+export const EndScreen = ({score}) => {
   const GoToNewGame = () => {
   };
   
   const GoToGameModes = () => {
   };
 
-  useEffect(() => {
-    setCounter(0);
-  }, []);
-
-  let EndScoreText;
-  switch (counter) {
+  let EndScoreText; //add more stuff
+  switch (score) {
     case 0:
       EndScoreText = 'Wow...';
       break;
@@ -33,7 +26,7 @@ const MyPage: React.FC = () => {
   return (
     <View style={styles.container}>
       <View style={styles.textBox}>
-        <Text style={styles.largeText}>{counter}/10</Text>
+        <Text style={styles.largeText}>{score}/10</Text>
         <Text style={styles.smallText}>{EndScoreText}</Text>
       </View>
       <TouchableOpacity style={styles.button} onPress={GoToNewGame}>
@@ -46,4 +39,46 @@ const MyPage: React.FC = () => {
   );
 };
 
-export default MyPage;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#312D2D',
+  },
+  textBox: {
+    width: '80%',
+    height: '30%',
+    backgroundColor: '#1B1B1F',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 40,
+  },
+  largeText: {
+    fontSize: 80,
+    fontWeight: 'bold',
+    color: '#fff',
+    textAlign: 'center',
+  },
+  smallText: {
+    fontSize: 24,
+    color: '#fff',
+    textAlign: 'center',
+    marginTop: 10,
+  },
+  button: {
+    width: '80%',
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: '#1B1B1F',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 40,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+});
